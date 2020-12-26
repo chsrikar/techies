f20c5513dc9d2c4af44124981840dc33498a32d1
chsrikar <76607442+chsrikar@users.noreply.github.com>
2020-12-26 11:03:13 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe6c4ebe4ec37001fad12dc",
+				"5fe6c4ebe4ec37001fad12de"
+			]
+		},
+		"5fe6c4ebe4ec37001fad12dc": {
+			"id": "5fe6c4ebe4ec37001fad12dc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe6c4ebe4ec37001fad12de": {
+			"id": "5fe6c4ebe4ec37001fad12de",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
